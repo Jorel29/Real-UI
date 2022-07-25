859991390986f56ea6eda8e71e662064f9d3d797
Jorel Huerto <jjhue29@gmail.com>
2022-07-25 16:33:37 -0700

    Init

---
+++ Source/RealUI/RealUI.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RealUI.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RealUI, "RealUI" );
---
+++ Source/RealUI/RealUIGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RealUIGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REALUI_API ARealUIGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
