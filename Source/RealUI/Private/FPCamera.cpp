388d2e1e07aa8e266e2a80f082f2a8a797e667bf
Jorel Huerto <jjhue29@gmail.com>
2022-08-03 13:57:50 -0700

    FPCamera Class

---
+++ Source/RealUI/Private/FPCamera.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPCamera.h"
+
+// Sets default values
+AFPCamera::AFPCamera()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFPCamera::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFPCamera::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AFPCamera::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
